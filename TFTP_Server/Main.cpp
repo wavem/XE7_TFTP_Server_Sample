c0f2fe27c759f0b2af3347bc60e524e20988dcae
WAVE <fenix24@naver.com>
2024-01-18 13:32:00 +0900

    [Work] Default Work
     - Basic Functions.,.

--- TFTP_Server/Main.cpp
+++ TFTP_Server/Main.cpp
@@ -6,11 +6,26 @@
 #include "Main.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+#pragma link "AdvMemo"
 #pragma resource "*.dfm"
 TFormMain *FormMain;
 //---------------------------------------------------------------------------
 __fastcall TFormMain::TFormMain(TComponent* Owner)
 	: TForm(Owner)
 {
+	InitProgram();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::InitProgram() {
+
+
+	PrintMsg(L"Init Complete");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::PrintMsg(UnicodeString _str) {
+	int t_Idx = memo->Lines->Add(_str);
+    memo->SetCursor(0, t_Idx);
 }
 //---------------------------------------------------------------------------
--- TFTP_Server/Main.h
+++ TFTP_Server/Main.h
@@ -7,13 +7,27 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include "AdvMemo.hpp"
+#include <IdBaseComponent.hpp>
+#include <IdCmdTCPServer.hpp>
+#include <IdComponent.hpp>
+#include <IdCustomTCPServer.hpp>
+#include <IdExplicitTLSClientServerBase.hpp>
+#include <IdFTPServer.hpp>
+#include <IdTCPServer.hpp>
 //---------------------------------------------------------------------------
 class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
+	TAdvMemo *memo;
+	TIdFTPServer *TFTP_Server;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFormMain(TComponent* Owner);
+
+public: // START
+	void __fastcall InitProgram();
+    void __fastcall PrintMsg(UnicodeString _str);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TFormMain *FormMain;
